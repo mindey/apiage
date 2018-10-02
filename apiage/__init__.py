85af9d0c9c40a544b988e2b05468c0456ed008ff
Mindey <mindey@qq.com>
2018-10-02 14:42:53 +0000

    upd

--- apiage/__init__.py
+++ apiage/__init__.py
@@ -27,59 +27,65 @@ def get(endpoint,
                     data = requests.get(endpoint).json()
             except:
 
+                data = None
+
+                # Sleep
                 if callable(sleep):
                     s = sleep()
                 else:
                     s = sleep
 
-                logging.log(20, 'Sleeping for {} seconds'.format(s))
+                # logging.log(20, 'Sleeping for {} seconds'.format(s))
+                print('Sleeping for {} seconds'.format(s))
 
                 time.sleep(s)
         else:
             break
 
-        if callable(pause):
-            t = pause()
-        else:
-            t = pause
-
-        if t:
-            time.sleep(t)
-
-        # count_key
-        if callable(count_key):
-            if not count_key(data):
+        if data:
+            # count_key
+            if callable(count_key):
+                if not count_key(data):
+                    return results
+            elif not data.get(count_key):
                 return results
-        elif not data.get(count_key):
-            return results
 
-        if not silent:
+            if not silent:
 
-            if 'bar' not in locals():
+                if 'bar' not in locals():
 
-                if callable(count_key):
-                    chunk = len(results_key(data))
-                    bar = Bar('API Pages:', max=round(count_key(data)/chunk))
-                elif count_key in data.keys():
-                    chunk = len(data.get(results_key))
-                    bar = Bar('API Pages:', max=round(data.get(count_key)/chunk))
+                    if callable(count_key):
+                        chunk = len(results_key(data))
+                        bar = Bar('API Pages:', max=round(count_key(data)/chunk))
+                    elif count_key in data.keys():
+                        chunk = len(data.get(results_key))
+                        bar = Bar('API Pages:', max=round(data.get(count_key)/chunk))
 
-            bar.next()
+                bar.next()
 
-        if callable(results_key):
-            if results_key(data):
-                results.extend(results_key(data))
-        elif results_key in data.keys():
-            results.extend(data[results_key])
+            if callable(results_key):
+                if results_key(data):
+                    results.extend(results_key(data))
+            elif results_key in data.keys():
+                results.extend(data[results_key])
 
-        if callable(next_key):
-            endpoint = next_key(data)
-        elif next_key not in data.keys():
-            break
-        elif not data.get(next_key):
-            break
-        else:
-            endpoint = data[next_key]
+            if callable(next_key):
+                endpoint = next_key(data)
+            elif next_key not in data.keys():
+                break
+            elif not data.get(next_key):
+                break
+            else:
+                endpoint = data[next_key]
+
+            # Pause
+            if callable(pause):
+                t = pause()
+            else:
+                t = pause
+
+            if t:
+                time.sleep(t)
 
     if not silent:
         bar.finish()
