840ca51e4db1e27d3f76f765ebe3966c3e4aa149
Mindey <mindey@qq.com>
2018-10-02 12:47:33 +0000

    make parameters callable=

--- apiage/__init__.py
+++ apiage/__init__.py
@@ -16,26 +16,45 @@ def get(endpoint,
 
     while True:
 
-        try:
-            data = requests.get(endpoint).json()
-        except:
-            logging.log('Sleeping for {} seconds'.format(sleep_seconds))
-            time.sleep(sleep_seconds)
+        if endpoint:
+            try:
+                data = requests.get(endpoint).json()
+            except:
+                logging.log('Sleeping for {} seconds'.format(sleep_seconds))
+                time.sleep(sleep_seconds)
+        else:
+            break
 
 
-        if not data.get(count_key):
+        # count_key
+        if callable(count_key):
+            if not count_key(data):
+                return results
+        elif not data.get(count_key):
             return results
 
         if not silent:
-            if count_key in data.keys() and 'bar' not in locals():
-                chunk = len(data.get(results_key))
-                bar = Bar('API Pages:', max=round(data.get(count_key)/chunk))
+
+            if 'bar' not in locals():
+
+                if callable(count_key):
+                    chunk = len(results_key(data))
+                    bar = Bar('API Pages:', max=round(count_key(data)/chunk))
+                elif count_key in data.keys():
+                    chunk = len(data.get(results_key))
+                    bar = Bar('API Pages:', max=round(data.get(count_key)/chunk))
 
             bar.next()
 
-        if results_key in data.keys():
+        if callable(results_key):
+            if results_key(data):
+                results.extend(results_key(data))
+        elif results_key in data.keys():
             results.extend(data[results_key])
-        if next_key not in data.keys():
+
+        if callable(next_key):
+            endpoint = next_key(data)
+        elif next_key not in data.keys():
             break
         elif not data.get(next_key):
             break
